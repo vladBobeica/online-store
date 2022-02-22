a26c80fc15094e2dee61113a76b0eb9516c8ade4
Vlad Bobeica <vlad.bobeica@outlook.com>
2022-02-22 17:28:16 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+const models = require("./models/models");
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const router = require("./routes/index");
+const errorHandler = require("./middleware/ErrorHandllerMiddleware");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(fileUpload({}));
+
+app.use("/api", router);
+
+app.use(errorHandler);
+
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Working!!!" });
+// });
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
