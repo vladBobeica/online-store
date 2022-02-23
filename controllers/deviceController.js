ab47d02200593302b1c8a7e09a6fcd5ca9394f0b
Vlad Bobeica <vlad.bobeica@outlook.com>
2022-02-23 13:54:25 +0100

    add device endpoint filter

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -25,7 +25,27 @@ class DeviceController {
     }
   }
 
-  async getAll(req, res) {}
+  async getAll(req, res) {
+    const { brandId, typeId } = req.query;
+    let devices;
+
+    if (!brandId && !typeId) {
+      devices = await Device.findAll();
+    }
+    if (brandId && !typeId) {
+      devices = await Device.findAll({ where: { brandId } });
+    }
+
+    if (!brandId && typeId) {
+      devices = await Device.findAll({ where: { typeId } });
+    }
+
+    if (brandId && typeId) {
+      devices = await Device.findAll({ where: { brandId, typeId } });
+    }
+
+    return res.json(devices);
+  }
 
   async getOne(req, res) {}
 }
--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/ErrorHandllerMiddleware");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+
 app.use(fileUpload({}));
 
 app.use("/api", router);
